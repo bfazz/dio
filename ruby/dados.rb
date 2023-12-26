6430249e32a95b095c134ce72a2e77007ce5e687
bruno fazz brunofazz80@gmail.com <brunofazz80@gmail.com>
2023-12-26 15:05:44 -0300

    dados

---
+++ ruby/dados.rb
@@ -0,0 +1,8 @@
+print "Informe seu nome "
+nome = gets.chomp
+print "Informe seu sobrenome "
+sobrenome = gets.chomp
+print "Informe sua idade "
+idade = gets.chomp.to_i
+
+puts "#{nome} #{sobrenome} #{idade}"
